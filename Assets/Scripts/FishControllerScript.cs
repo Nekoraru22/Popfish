64fb603f6d6fe839013c89a78320f1a783870f55
Neko <izangandiaruiz@gmail.com>
2025-01-25 10:08:31 +0100

    FishController

---
+++ Assets/Scripts/FishControllerScript.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class FishControllerScript : MonoBehaviour
+{
+    public GameObject fish;
+    
+    public void SetStun() {
+        // Rebotar e inmovilizar al pez durante 2 segundos
+    }
+
+    public void SetPoison() {
+        // Menos salto y/o velocidad durante 5 segundos
+    }
+
+    public void SetDrowned() {
+        // Invertir controles hasta recargar el oxígeno
+    }
+
+    public void RestoreDrowned() {
+        // Restaurar controles
+    }
+
+    public void LoseBubbles(int bubbles) {
+        // Restar burbujas
+    }
+
+    public void RefillBubbles(int bubbles) {
+        // Sumar burbujas
+        // Si -1, sumar todas las burbujas
+    }
+
+    public void DisableColliders() {
+        // Desactivar colisiones
+    }
+
+    public void EnableColliders() {
+        // Activar colisiones
+    }
+}
