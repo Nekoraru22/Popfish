63f4612ce961154dd3b8db479304d03d63128a7c
CcWhyNot <carloscasadolopez53@gmail.com>
2025-01-25 13:17:06 +0100

    Script logico fish excepto bubble y collider de fish

--- Assets/Scripts/FishControllerScript.cs
+++ Assets/Scripts/FishControllerScript.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 public class FishControllerScript : MonoBehaviour
 {
     public GameObject fish;
+    private FishScript fishScript;
+
+    private void Start()
+    {
+        fishScript = fish.GetComponent<FishScript>();
+    }
     //fish = GameObject.Find("fish").GetComponent<FishScript>();
 
     public void SetStun() {
         // Rebotar e inmovilizar al pez durante 2 segundos
+        fishScript.setStunned();
     }
 
     public void SetPoison() {
         // Menos salto y/o velocidad durante 5 segundos
+        fishScript.setPosion();
     }
 
     public void SetDrowned() {
         // Invertir controles hasta recargar el oxígeno
+        fishScript.SetInverseControls();
     }
 
     public void RestoreDrowned() {
         // Restaurar controles
+        fishScript.SetNormalControls();
     }
 
     public void LoseBubbles(int bubbles) {
         // Restar burbujas
-
     }
 
     public void RefillBubbles(int bubbles) {
