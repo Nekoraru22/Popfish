6198b7f1a535d4e099130a5713232a63d3b30bd3
Neko <izangandiaruiz@gmail.com>
2025-01-26 10:21:13 +0100

    meow

--- Assets/Scripts/PoisonousWaterScript.cs
+++ Assets/Scripts/PoisonousWaterScript.cs
@@ -5,6 +5,7 @@ public class PoisonousWaterScript : MonoBehaviour
     public FishControllerScript fishControllerScript;
 
     private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("Player entered the poisonous water");
         if (collision.gameObject.CompareTag("Fish")) {
             Debug.Log("Player entered the poisonous water");
             fishControllerScript.SetPoison();
